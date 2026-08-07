7b803b12ec08087d017e6e2b8a92396d127ce0b3
konradkarso-droid <konradkarso@gmail.com>
2026-08-07 23:25:07 +0300

    Update MemoryDatabase.kt

--- app/src/main/java/com/uroboros/memory/MemoryDatabase.kt
+++ app/src/main/java/com/uroboros/memory/MemoryDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Sticker::class], version = 3, exportSchema = false)
+@Database(entities = [Sticker::class], version = 4, exportSchema = false)
 abstract class MemoryDatabase : RoomDatabase() {
     abstract fun stickerDao(): StickerDao
     companion object {
